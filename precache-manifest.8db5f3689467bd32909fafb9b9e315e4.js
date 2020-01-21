ce97b4f90596f27f2e7f02901c58cb1a718afbbc
103071073 <josmyfaure@gmail.com>
2020-01-21 21:21:56 +0800

    Updates

---
+++ precache-manifest.8db5f3689467bd32909fafb9b9e315e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "e2ddc62c77fbf63600de",
+    "url": "/resume/static/js/main.62d98c4f.chunk.js"
+  },
+  {
+    "revision": "cdbabc34896f1f48ea90",
+    "url": "/resume/static/js/2.ecd37079.chunk.js"
+  },
+  {
+    "revision": "e2ddc62c77fbf63600de",
+    "url": "/resume/static/css/main.57160bf8.chunk.css"
+  },
+  {
+    "revision": "4693175b127af52eb9d701dfa25d00c4",
+    "url": "/resume/index.html"
+  }
+];
