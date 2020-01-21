4a8085a11cf42fc0f5b6ac339e324645c9af3b25
103071073 <josmyfaure@gmail.com>
2020-01-21 21:33:19 +0800

    no

---
+++ src/components/Portfolio.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react';
+export default class Porfolio extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="portfolio">
+      
+  </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,182 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Haw-Ya, Yeh",
+    "role": "Financial Engineer (MS)",
+    "roleDescription": "Financial Engineer (MS) focusing on using Mathematical methods to solve Financial problems, \
+    especially those related to Investment Strategizing and Risk Management.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"#",
+          "className":"fa fa-linkedin"
+        },
+        
+      ],
+    "aboutme":"I'm a persistent, detail-oriented, enthusiastic, responsible. \
+    An independent as well as a team worker.  \
+    Once I have a clear goal, all my efforts will be aligned to achieve it. \
+    I worked previously as an Intern at the Shanghai Stock Exchange and the Bank of China. \
+     Currently, I am doing another Internship at Eastpring Investments.",
+    "className":"fa fa-map-marker",
+
+    "address":"Taipei, Taiwan",
+    "website":"https://103071073.github.io/resume",
+    "mail":"Email: josmyfaure@gmail.com",
+    "education":[
+      {
+        "UniversityName":"National Taiwan University",
+        "specialization":"Financial Engineering",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2020",
+        "Achievements":"Master of Science"
+      },
+      {
+        "UniversityName":"National Tsing Hua University",
+        "specialization":"Quantitative Finance",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2018",
+        "Achievements":"Bachelor Degree"
+      }
+    ],
+    "work":[
+
+      {
+        "CompanyName":"Eastpring Investments",
+        "specialization":"Research and Investment Intern",
+        "MonthOfLeaving":"Ongoing",
+        // "YearOfLeaving":"2017",
+        "Achievements":"The bulk of my work consists in writing research reports about various companies and assisting the Fund Managers in their trading\
+        decisions."
+      },
+      {
+        "CompanyName":"Shanghai Stock Exchange",
+        "specialization":"Securities Intern",
+        "MonthOfLeaving":"August",
+        "YearOfLeaving":"2017",
+        "Achievements":"Our main project and case study was about SWAP and market\
+        research in China."
+      },
+      {
+        "CompanyName":"Bank of China",
+        "specialization":"Associate Intern",
+        "MonthOfLeaving":"August",
+        "YearOfLeaving":"2016",
+        "Achievements":"Cross-Department Internship program where I had to work and gain knowledge and know-how from \
+        the Business Center, Information Technology, Personal Banking and Corporate Finance Departments."
+      }
+    ],
+
+    "certifications":[
+      {
+        "certName":"Deep Learning Specialization",
+        "organization":"deeplearning.ai",
+        "issueMonth":"November",
+        "issueYear":"2019",
+        "link": "https://www.coursera.org/account/accomplishments/specialization/certificate/NKKHVW8HEP7Z",
+      },
+      {
+        "certName":"Data-Driven Decision Making",
+        "organization":"PwC",
+        "issueMonth":"May",
+        "issueYear":"2019",
+        "link": "https://www.coursera.org/account/accomplishments/certificate/5FL2H58FRQ6J",
+      },
+      {
+        "certName":"SQL for Data Science",
+        "organization":"University of California, Davis",
+        "issueMonth":"May",
+        "issueYear":"2019",
+        "link": "https://www.coursera.org/account/accomplishments/certificate/R9RJ7B7AD7RD",
+      },
+
+    ],
+
+    "skills":[
+      {
+        "skillname":"Stata",
+        "stylename": "stata",
+      },
+      {
+        "skillname":"Matlab",
+        "stylename":"matlab",
+      },
+  
+      {
+        "skillname":"Python",
+        "stylename": "python",
+      },
+      {
+        "skillname":"Excel (VBA)",
+        "stylename": "excel",
+      }
+    ],
+
+    "languages":[
+      {
+        "skillname":"Chinese",
+        "stylename": "chinese",
+      },
+      {
+        "skillname":"English",
+        "stylename":"english",
+      },
+  
+      {
+        "skillname":"Spanish",
+        "stylename": "spanish",
+      }
+    ],
+    "portfolio":[
+      // {
+      //   "name":"Online Face recognition and emotion detection",
+      //   "description":"",
+      //   "imgurl":"images/portfolio/face.jpg",
+      //   "url": "http://www.geekperimeter.com/face_recognition/"
+      // },
+      // {
+      //   "name":"Camera Security with motion detection and face recognition (built with with raspberry pi)",
+      //   "description":"",
+      //   "imgurl":"images/portfolio/raspberry.jpg",
+      //   "url": "https://github.com/joslefaure/CS_finalProject"
+
+      // },
+      // {
+      //   "name":"Financial data crawling and analysis",
+      //   "description":"",  
+      //   "imgurl":"images/portfolio/financial.jpg",
+      //   "url": "https://github.com/joslefaure/ML_Final_Project"
+      // },
+      // {
+      //   "name":"Online Stock Prediction",
+      //   "description":"",
+      //   "imgurl":"images/portfolio/stock.jpg",
+      //   "url": "http://www.geekperimeter.com/stock_prediction/"
+      // },
+      
+    ],
+    "testimonials":[
+      {
+        "description":"Rule No.1: Never lose money. Rule No.2: Never forget rule No.1.",
+        "name":"Warren Buffet"
+      },
+      {
+        "description":"Never spend your money before you have it.",
+        "name":"Thomas Jefferson"
+      },
+      {
+        "description":"Don’t let the fear of losing be greater than the excitement of winning.",
+        "name":"Robert Kiyosaki"
+      }
+    ],
+
+    "contact":[
+      {
+        "name":"",
+        "name":"",
+        "message":""
+      }
+    ]
+
+  }
+  
+  export default resumeData
